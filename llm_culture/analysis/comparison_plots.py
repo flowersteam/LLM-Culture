e9af1119d2bbbab4bb9e0d02f9d7b8ddeab19bde
corentinlger <corentin.lger@gmail.com>
2024-02-21 14:47:59 +0100

    Add comparison analysis between different experiments

---
+++ llm_culture/analysis/comparison_plots.py
@@ -0,0 +1,221 @@
+import os 
+import numpy as np
+import matplotlib.pyplot as plt 
+
+
+def compare_init_generation_similarity_evolution(data, plot, saving_folder=None):
+    plt.figure(figsize=(10, 6))
+    plt.title('Evolution of similarity with the initial generation')
+    plt.xlabel('Generations')
+    plt.ylabel('Similarity with first generation')
+    
+    max_num_ticks = 0 
+
+    for folder in data:
+        num_ticks = data[folder]['between_gen_similarity_matrix'].shape[0]
+        if num_ticks > max_num_ticks:
+            max_num_ticks = num_ticks
+            x_ticks_space = data[folder]['x_ticks_space']
+
+    plt.ylim(0, 1)
+    plt.xticks(range(0, max_num_ticks, x_ticks_space))
+    plt.yticks(np.linspace(0, 1, 11))
+    plt.grid()
+
+    for folder in data:
+        num_points = data[folder]['between_gen_similarity_matrix'].shape[0]
+        value = data[folder]['between_gen_similarity_matrix'][0, 1:]
+        plt.plot(range(1, num_points), value, label=folder)
+
+    plt.legend()
+    
+    if saving_folder:
+        saving_name = '/similarity_first_gen_comparison.png'
+        os.makedirs(f"Results/Comparisons/{saving_folder}", exist_ok=True)
+        plt.savefig(f"Results/Comparisons/{saving_folder}/{saving_name}")
+        print(f"Saved {saving_name}")
+    
+    if plot:
+        plt.show()
+    
+def compare_within_generation_similarity_evolution(data, plot, saving_folder=None):
+    plt.figure(figsize=(10, 6))
+    plt.title('Evolution of similarity with the initial generation')
+    plt.xlabel('Generations')
+    plt.ylabel('Similarity within generations')
+    
+    max_num_ticks = 0 
+
+    for folder in data:
+        num_ticks = data[folder]['between_gen_similarity_matrix'].shape[0]
+        if num_ticks > max_num_ticks:
+            max_num_ticks = num_ticks
+            x_ticks_space = data[folder]['x_ticks_space']
+
+    plt.ylim(-0.1, 1.1)
+    plt.xticks(range(0, max_num_ticks, x_ticks_space))
+    plt.yticks(np.linspace(0, 1, 11))
+    plt.grid()
+
+    for folder in data:
+        value = np.diag(data[folder]['between_gen_similarity_matrix'])
+        plt.plot(value, label=folder, alpha=0.7)
+
+    plt.legend()
+    
+    if saving_folder:
+        saving_name = '/similarity_within_gen_comparison.png'
+        os.makedirs(f"Results/Comparisons/{saving_folder}", exist_ok=True)
+        plt.savefig(f"Results/Comparisons/{saving_folder}/{saving_name}")
+        print(f"Saved {saving_name}")
+    
+    if plot:
+        plt.show()
+
+
+def compare_successive_generations_similarities(data, plot, saving_folder=None):
+    plt.figure(figsize=(10, 6))
+    plt.title('Evolution of similarity with previous generation')
+    plt.xlabel('Generations')
+    plt.ylabel('Similarity with first generation')
+    
+    max_num_ticks = 0 
+
+    for folder in data:
+        num_ticks = data[folder]['between_gen_similarity_matrix'].shape[0]
+        if num_ticks > max_num_ticks:
+            max_num_ticks = num_ticks
+            x_ticks_space = data[folder]['x_ticks_space']
+
+    plt.ylim(-0.1, 1.1)
+    plt.xticks(range(0, max_num_ticks, x_ticks_space))
+    plt.yticks(np.linspace(0, 1, 11))
+    plt.grid()
+
+    for folder in data:
+        between_gen_similarity_matrix = data[folder]['between_gen_similarity_matrix']
+        successive_sim = [between_gen_similarity_matrix[i, i+1] for i in range(between_gen_similarity_matrix.shape[0] - 1)]
+        plt.plot(successive_sim, label=folder, alpha=0.7)
+
+    plt.legend()
+    
+    if saving_folder:
+        saving_name = '/similarity_successive_gen_comparison.png'
+        os.makedirs(f"Results/Comparisons/{saving_folder}", exist_ok=True)
+        plt.savefig(f"Results/Comparisons/{saving_folder}/{saving_name}")
+        print(f"Saved {saving_name}")
+    
+    if plot:
+        plt.show()
+
+
+def compare_positivity_evolution(data, plot, saving_folder=None):
+    plt.figure(figsize=(10, 6))
+    plt.title('Evolution of positivity within generations')
+    plt.xlabel('Generations')
+    plt.ylabel('Similarity with first generation')
+    
+    max_num_ticks = 0 
+
+    for folder in data:
+        num_ticks = data[folder]['between_gen_similarity_matrix'].shape[0]
+        if num_ticks > max_num_ticks:
+            max_num_ticks = num_ticks
+            x_ticks_space = data[folder]['x_ticks_space']
+
+    plt.ylim(-1, 1)
+    plt.xticks(range(0, max_num_ticks, x_ticks_space))
+    plt.yticks(np.linspace(-1, 1, 11))
+
+    plt.grid()
+
+    for folder in data:
+        gen_positivities = []
+        for gen_polarities in data[folder]['polarities']:
+            gen_positivities.append(np.mean(gen_polarities))
+        plt.plot(gen_positivities, label=folder, alpha=0.7)
+
+    plt.legend()
+    
+    if saving_folder:
+        saving_name = '/positivity_gen_comparison.png'
+        os.makedirs(f"Results/Comparisons/{saving_folder}", exist_ok=True)
+        plt.savefig(f"Results/Comparisons/{saving_folder}/{saving_name}")
+        print(f"Saved {saving_name}")
+    
+    if plot:
+        plt.show()
+        
+
+def compare_subjectivity_evolution(data, plot, saving_folder=None):
+    plt.figure(figsize=(10, 6))
+    plt.title('Evolution of subjectivity within generations')
+    plt.xlabel('Generations')
+    plt.ylabel('Similarity with first generation')
+    
+    max_num_ticks = 0 
+
+    for folder in data:
+        num_ticks = data[folder]['between_gen_similarity_matrix'].shape[0]
+        if num_ticks > max_num_ticks:
+            max_num_ticks = num_ticks
+            x_ticks_space = data[folder]['x_ticks_space']
+
+    plt.ylim(0, 1)
+    plt.xticks(range(0, max_num_ticks, x_ticks_space))
+    plt.yticks(np.linspace(0, 1, 11))
+
+    plt.grid()
+
+    for folder in data:
+        gen_positivities = []
+        for gen_polarities in data[folder]['subjectivities']:
+            gen_positivities.append(np.mean(gen_polarities))
+        plt.plot(gen_positivities, label=folder, alpha=0.7)
+
+    plt.legend()
+    
+    if saving_folder:
+        saving_name = '/subjectivity_gen_comparison.png'
+        os.makedirs(f"Results/Comparisons/{saving_folder}", exist_ok=True)
+        plt.savefig(f"Results/Comparisons/{saving_folder}/{saving_name}")
+        print(f"Saved {saving_name}")
+    
+    if plot:
+        plt.show()
+
+
+def compare_creativity_evolution(data, plot, saving_folder=None):
+    plt.figure(figsize=(10, 6))
+    plt.title('Evolution of creativity within generations')
+    plt.xlabel('Generations')
+    plt.ylabel('Similarity with first generation')
+    
+    max_num_ticks = 0 
+
+    for folder in data:
+        num_ticks = data[folder]['between_gen_similarity_matrix'].shape[0]
+        if num_ticks > max_num_ticks:
+            max_num_ticks = num_ticks
+            x_ticks_space = data[folder]['x_ticks_space']
+
+    plt.ylim(0, 1)
+    plt.xticks(range(0, max_num_ticks, x_ticks_space))
+    plt.yticks(np.linspace(0, 1, 11))
+
+    plt.grid()
+
+    for folder in data:
+        gen_creativities = [np.mean(gen_creativity) for gen_creativity in data[folder]['creativities']]
+        plt.plot(gen_creativities, label=folder, alpha=0.7)
+
+    plt.legend()
+    
+    if saving_folder:
+        saving_name = '/creativity_gen_comparison.png'
+        os.makedirs(f"Results/Comparisons/{saving_folder}", exist_ok=True)
+        plt.savefig(f"Results/Comparisons/{saving_folder}/{saving_name}")
+        print(f"Saved {saving_name}")
+    
+    if plot:
+        plt.show()
---
+++ run_comparison_analysis.py
@@ -0,0 +1,69 @@
+import argparse
+
+from llm_culture.analysis.utils import get_stories, get_plotting_infos, preprocess_stories, get_similarity_matrix
+from llm_culture.analysis.utils import compute_between_gen_similarities, get_polarities_subjectivities, compute_creativity_indexes
+from llm_culture.analysis.comparison_plots import *
+
+
+def main_analysis(folders, plot=False):
+    saving_folder = '-'.join(os.path.basename(folder) for folder in folders)
+    data = {}
+        
+    for folder in folders:
+        stories = get_stories(folder)
+        n_gen, n_agents, x_ticks_space = get_plotting_infos(stories)
+        flat_stories, keywords, stem_words = preprocess_stories(stories)
+        similarity_matrix = get_similarity_matrix(flat_stories)
+        between_gen_similarity_matrix = compute_between_gen_similarities(similarity_matrix, n_gen, n_agents)
+        polarities, subjectivities = get_polarities_subjectivities(stories)
+
+        data[folder] = {
+            'stories': stories,
+            'n_gen': n_gen,
+            'n_agents': n_agents,
+            'x_ticks_space': x_ticks_space,
+            'flat_stories': flat_stories,
+            'keywords': keywords,
+            'stem_words': stem_words,
+            'similarity_matrix': similarity_matrix,
+            'between_gen_similarity_matrix': between_gen_similarity_matrix,
+            'polarities': polarities,
+            'subjectivities': subjectivities
+        }
+
+    
+    # Plot all the desired graphs :
+
+    compare_init_generation_similarity_evolution(data, plot, saving_folder)
+
+    compare_within_generation_similarity_evolution(data, plot, saving_folder)
+
+    compare_successive_generations_similarities(data, plot, saving_folder)
+
+    compare_positivity_evolution(data, plot, saving_folder)
+
+    compare_subjectivity_evolution(data, plot, saving_folder)
+
+    # Compute creativity at the end because it takes more time 
+    for folder in folders:
+        print(f"Computing creativity for {folder} ...")
+        creativities = compute_creativity_indexes(data[folder]['stories'])
+
+        data[folder]['creativities'] = creativities
+
+    compare_creativity_evolution(data, data, saving_folder)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Enter the names of the experiments separated by spaces
+    parser.add_argument("--dirs", type=str, default="Chain_20 Chain_50")
+    parser.add_argument("--plot", action="store_true")
+    args = parser.parse_args()
+
+    analyzed_dirs = args.dirs.split(' ')
+    dirs_list = [f"Results/{dir_name}" for dir_name in analyzed_dirs]
+
+    print(f"\nLaunching analysis on the {args.dirs} results")
+    print(f"plot = {args.plot}")
+    main_analysis(dirs_list, args.plot)
