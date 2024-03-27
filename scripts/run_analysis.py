343ffac19ea36019500ef18539e30f1300b42cda
corentinlger <corentin.lger@gmail.com>
2024-03-27 10:23:38 +0100

    Move executable files in scripts and refactor

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -1,6 +1,4 @@
-import json
 import argparse
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 from llm_culture.analysis.utils import get_stories, get_plotting_infos, preprocess_stories, get_similarity_matrix
 from llm_culture.analysis.utils import compute_between_gen_similarities, get_polarities_subjectivities, get_creativity_indexes
@@ -26,42 +24,25 @@ def main_analysis(folder, font_sizes = {'ticks': 12, 'labels': 14, 'title': 16},
 
 
     # Plot all the desired graphs :
-
-
     for seed in range(n_seeds):
         print(seed)
         similarity_matrix = all_seeds_similarity_matrix[seed]
         between_gen_similarity_matrix = all_seeds_between_gen_similarity_matrix[seed]
-
-        
         stem_words = all_seeds_stem_words[seed]
 
-        plot_similarity_matrix(similarity_matrix, n_gen, n_agents, folder, plot, seed = seed, sizes= font_sizes)
-        
-        
-        plot_between_gen_similarities(between_gen_similarity_matrix, folder, plot, x_ticks_space, seed = seed, sizes= font_sizes)
+        # Individual plots for the current seed 
+        plot_similarity_matrix(similarity_matrix, n_gen, n_agents, folder, plot, seed=seed, sizes=font_sizes)
+        plot_between_gen_similarities(between_gen_similarity_matrix, folder, plot, x_ticks_space, seed=seed, sizes=font_sizes)
+        plot_word_chains(stem_words, folder, plot, x_ticks_space, seed=seed, sizes=font_sizes)
+        plot_similarity_graph(between_gen_similarity_matrix, folder, plot, seed=seed, sizes=font_sizes)
 
-        plot_word_chains(stem_words, folder, plot, x_ticks_space, seed = seed, sizes= font_sizes)
-
-
-        plot_similarity_graph(between_gen_similarity_matrix, folder, plot, seed = seed, sizes= font_sizes)
-
-
-    plot_init_generation_similarity_evolution(all_seeds_between_gen_similarity_matrix, folder, plot, x_ticks_space, sizes= font_sizes)
-
-    plot_within_gen_similarities(all_seeds_between_gen_similarity_matrix, folder, plot, x_ticks_space, sizes= font_sizes)
-
-    plot_successive_generations_similarities(all_seeds_between_gen_similarity_matrix, folder, plot, x_ticks_space, sizes= font_sizes)
-
-    plot_positivity_evolution(all_seeds_polarities, folder, plot, x_ticks_space, sizes= font_sizes)
-
-    plot_subjectivity_evolution(all_seeds_subjectivities, folder, plot, x_ticks_space, sizes= font_sizes)
-
-
-
-    # TODO : Change the function because 1 = low creativity and 0 = high --> DONE 
-    plot_creativity_evolution(all_seeds_creativities, folder, plot, x_ticks_space, sizes= font_sizes)
-
+    # Plots comparing all the seeds
+    plot_init_generation_similarity_evolution(all_seeds_between_gen_similarity_matrix, folder, plot, x_ticks_space, sizes=font_sizes)
+    plot_within_gen_similarities(all_seeds_between_gen_similarity_matrix, folder, plot, x_ticks_space, sizes=font_sizes)
+    plot_successive_generations_similarities(all_seeds_between_gen_similarity_matrix, folder, plot, x_ticks_space, sizes=font_sizes)
+    plot_positivity_evolution(all_seeds_polarities, folder, plot, x_ticks_space, sizes=font_sizes)
+    plot_subjectivity_evolution(all_seeds_subjectivities, folder, plot, x_ticks_space, sizes=font_sizes)
+    plot_creativity_evolution(all_seeds_creativities, folder, plot, x_ticks_space, sizes=font_sizes)
 
 
 if __name__ == "__main__":
