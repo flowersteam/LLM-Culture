efa46dfc9124510806ab0a2a3d9f0815c1bc3ad0
corentinlger <corentin.lger@gmail.com>
2024-05-03 14:49:06 +0200

    Fix zipping prompts variables error

--- run_overnight.py
+++ run_overnight.py
@@ -1,18 +1,13 @@
 import argparse
 import run_simulation
 from pathlib import Path
-from tqdm import trange
 
 
 ## VARIABLES
 n_agents_var = [2, 5, 10]
 
-
 prompt_init_var = ['War of the Ghosts inspiration', 'Darwinism inspiration', 'War of the Ghosts rephrase', 'Darwinism rephrase']
 prompt_update_var = ['inspiration_multi', 'inspiration_multi', 'rephrase_multi', 'rephrase_multi']
-prompts_var = zip(prompt_init_var, prompt_update_var)
-
-
 
 ## CONSTANTS
 n_timesteps = 50   
@@ -26,19 +21,18 @@ access_url = 'https://ve-breathing-msgid-look.trycloudflare.com'
 
 ## SIMULATION LOOP
 for n_agents in n_agents_var:
-    for (prompt_init, prompt_update) in prompts_var:
-        print(n_agents, prompt_init, prompt_update)
+    print(f"\n{n_agents = }")
+
+    for (prompt_init, prompt_update) in zip(prompt_init_var, prompt_update_var):
+        print(f"\n{n_agents} agents - {prompt_init} - {prompt_update}")
             
         n_edges = (n_agents ** 2) // 2
         personality_list = ['Empty'] * n_agents
 
-
         ## MODIFY MODEL NAME HERE
         model = 'mistral-7b-instruct-v0.2.Q5_K_M.gguf'
 
-
         output_folder_path = f"{prompt_init}_{n_agents}Agents_50gen_{model}"
-
 
         args = argparse.Namespace()
         args.n_agents = n_agents
@@ -46,7 +40,6 @@ for n_agents in n_agents_var:
         args.prompt_init = prompt_init
         args.prompt_update = prompt_update
         args.format_prompt = "Empty"
-
 
         args.start_flag = None
         args.end_flag = None
